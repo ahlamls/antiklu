fb4365c13b72893b1d11fba3000c546916ee47af
ahlamls <fulldoang@gmail.com>
2020-05-21 20:40:23 +0700

    bisa repot saya

--- app/src/main/java/com/antiklu/aplikasi/SplashActivity.java
+++ app/src/main/java/com/antiklu/aplikasi/SplashActivity.java
@@ -33,6 +33,6 @@ public class SplashActivity extends AppCompatActivity {
                     finish();
                 }
             }
-        }, 2000L); //3000 L = 3 detik
+        }, 3000L); //3000 L = 3 detik
     }
 }
